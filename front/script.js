b1383821656bc3f39c7c03e8d57cd182447441cf
Ale Torquato <alexandretorquato846@gmail.com>
2026-04-13 17:42:10 -0400

    refiz o script pq tava dando erro também

--- front/script.js
+++ front/script.js
@@ -1,32 +1,11 @@
-function getColor(status) {
-    if (status === "ativa") return "green";
-    if (status === "cheia") return "red";
-    return "blue"
-}
+var map = L.map('map').setView([-21.2522, -52.0353], 14);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+}).addTo(map);
 
-
-var map = L.map('map').setView([-21.48, -51.53], 13);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
-    .addTo(map);
-
-//vou precisar dos dados do backend para adicionar no js
-
-fetch('')
-then(res => res.json())
-    .then(dados => {
-        dados.forEach(p => {
-            L.circleMarker([p.lat, p.lng], {
-                radius: 8,
-                color: getColor(p.status),
-                fillColor: getColor(p.status),
-                fillOpacity: 0.8
-            })
-                .addTo(map)
-                .bindPopup(`Status: ${p.status}<br>Bairro: ${p.bairro}`);
-        });
-    });
-
-//aqui vai entrar os casos de dengue, porém preciso dos dados do back tbm
-fetch('')
-    .then()
+// Exemplo de marcador
+L.marker([-21.2522, -52.0353])
+    .addTo(map)
+    .bindPopup("<b>Brasilândia - MS</b><br>Centro da cidade")
+    .openPopup();
